81e43ca0076bdcc89e8f9b5689d2287f6766f04c
Franco Parodi <franco.parodi@smeup.com>
2020-04-21 17:26:04 +0200

    Handle daemon cause emit is not allowed from button_callback

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -12,7 +12,8 @@ stop_event = threading.Event()
 daemon = threading.Thread()
 isDaemonStarted = False
 status = "0"
-secondsBetweenSensorRead = 10
+secondsBetweenGPIOStatus = 1
+gPIOEvent = False
 
 view = Blueprint("view", __name__)
 
@@ -26,20 +27,24 @@ def settings():
 
 @socketio.on('handleDaemon')
 def on_handleDaemon(data):
+    global gPIOEvent
     name=data['name']
     action=data['action']
 
     @copy_current_request_context
     def daemonProcess(name, status, stop_event):
-        # TODO really need daemon?
+        # Daemon needed cause emit from button_callback is not allowed
+        global gPIOEvent
         while not stop_event.is_set():
-            time.sleep(secondsBetweenSensorRead)
+            if gPIOEvent:
+                emit('daemonProcess', {'datetime': str(datetime.now()), 'name': name, 'status': status})
+                gPIOEvent = False
+            time.sleep(secondsBetweenGPIOStatus)
     
     def button_callback(channel):
         print("Button was pushed!")
-        global status
-        status = "1"
-        emit('daemonProcess', {'datetime': str(datetime.now()), 'name': name, 'status': status})
+        global gPIOEvent
+        gPIOEvent = True
 
     global isDaemonStarted
     if action == 'START':
@@ -50,10 +55,12 @@ def on_handleDaemon(data):
             GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback, bouncetime=2000) # Setup event on pin 10 rising edge
             daemon.__init__(target=daemonProcess, args=(name, status, stop_event), daemon=True)
             daemon.start()
+            gPIOEvent = False
             isDaemonStarted = True
     else:
         GPIO.cleanup() # Clean up
         stop_event.set()
         daemon.join()
         stop_event.clear()
+        gPIOEvent = False
         isDaemonStarted = False
